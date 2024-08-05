4e82b67ebd9b465855c5bf8e8e7b2aa92145731d
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-05 10:18:45 +0900

    Feat : 트레이트로 구현하는 타입을 반환하기

--- Trait/trait_test_main_2/src/main.rs
+++ Trait/trait_test_main_2/src/main.rs
@@ -6,8 +6,20 @@ fn main() {
         location: String::from("Pittsburgh, PA, USA"),
         author: String::from("Iceburgh"),
         content: String::from(
-            "The Pittsburgh Penguins once again are the best \ hockey team in the NHL.",)
+            "The Pittsburgh Penguins once again are the best hockey team in the NHL.",)
     };
 
     println!("New article available! {}", article.summarize());
 }
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        uesrname: String::frim("horse_ebooks");
+        content: String::from(
+            "of course, ay you probably already know, people",
+        ),
+        reply: false;
+        retweet:false;
+    }
+}
+
