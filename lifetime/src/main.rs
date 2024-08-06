08025121da934e05a74a67b8acc1d63e13831878
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-06 09:21:09 +0900

    Feat | 라이프타임 제네릭 같이 활용하기

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -15,3 +15,19 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
         y
     }
 }
+
+
+fn lognest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where T: Display
+{
+    println!("Announcement! {}", ann);
+    if(x.len() > y.len()){
+        x
+    } else {
+        y
+    }
+}
