62ec60f2b117400f46c8bb3d0d4fbe7a8b51ab14
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-21 20:48:58 +0900

    New | Rc와 RefCell을 같이 쓰는 예제

---
+++ review/rc_refcell_test/src/main.rs
@@ -0,0 +1,23 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+fn main() {
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(List::Cons(Rc::clone(&value), Rc::new(List::Nil)));
+
+    let b = List::Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = List::Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c afger = {:?}", c);
+}
